9fd82f7df6a60d14eaea60aed2315e97806a6ac6
Tunji <117839317+leem-dev@users.noreply.github.com>
2023-04-08 18:32:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("createSidebar").style.width = "250px";
+  document.getElementById("main-sidebar").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("createSidebar").style.width = "0";
+  document.getElementById("main-sidebar").style.marginRight = "0";
+}
